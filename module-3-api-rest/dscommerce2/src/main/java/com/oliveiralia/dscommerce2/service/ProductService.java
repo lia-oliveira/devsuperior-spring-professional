088700fe5c377e5e9b5983a335434921d1405494
lia-oliveira <dev.gillian@gmail.com>
2025-04-17 09:40:32 -0300

    Products paged

--- module-3-api-rest/dscommerce2/src/main/java/com/oliveiralia/dscommerce2/service/ProductService.java
+++ module-3-api-rest/dscommerce2/src/main/java/com/oliveiralia/dscommerce2/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.oliveiralia.dscommerce2.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +15,13 @@ public class ProductService {
 	
 	@Autowired
 	private ProductRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAll(Pageable pageable){
+		Page<Product> result = repository.findAll(pageable);
+		return result.map(x-> new ProductDTO(x));
+	}
+	
 	
 	@Transactional(readOnly = true)
 	public ProductDTO findById(Long id) {

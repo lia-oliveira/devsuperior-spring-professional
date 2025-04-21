dd7396ae2e56e0cd02438cce6d5a606dce891337
lia-oliveira <dev.gillian@gmail.com>
2025-04-21 19:23:21 -0300

    basic project

---
+++ module-4-jpa-sql-jpql/mypeopleapp/src/test/java/com/oliveiralia/mypeopleapp/MypeopleappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveiralia.mypeopleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypeopleappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

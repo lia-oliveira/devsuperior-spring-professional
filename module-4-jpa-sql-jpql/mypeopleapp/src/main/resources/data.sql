f8fcbd01b8085251e2c09332cd7d41084d47d393
lia-oliveira <dev.gillian@gmail.com>
2025-04-21 22:12:34 -0300

    seed da base de dados

---
+++ module-4-jpa-sql-jpql/mypeopleapp/src/main/resources/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO tb_department(name) VALUES ('Administração');
+INSERT INTO tb_department(name) VALUES ('Contabilidade');
+INSERT INTO tb_department(name) VALUES ('Desenvolvimento');
+INSERT INTO tb_department(name) VALUES ('Marketing');
+INSERT INTO tb_department(name) VALUES ('Vendas');
+INSERT INTO tb_department(name) VALUES ('Operações');
+
+
+INSERT INTO tb_person(name, salary) VALUES ('Maria de Lurdes da Silva', 10500.0);
+INSERT INTO tb_person(name, salary) VALUES ('Margareth Félix Amaral', 20500.0);
+INSERT INTO tb_person(name, salary) VALUES ('Lucas Rodrigues Crisantemo', 11250.0);
+INSERT INTO tb_person(name, salary) VALUES ('Maiara Motta', 20430.0);
+INSERT INTO tb_person(name, salary) VALUES ('Misna Soares Matos', 9480.0);
+INSERT INTO tb_person(name, salary) VALUES ('Diego Cabral Alves', 5400.0);
+INSERT INTO tb_person(name, salary) VALUES ('Iris Camil', 8700.0);
+
+
+
+
